ee5e130b108fde40bd65eee5f8c74e1d5b20fb97
Chunkie0 <davisdambis1@gmail.com>
2021-07-06 16:03:55 +0300

    initial commit

---
+++ migrate.py
@@ -0,0 +1,38 @@
+import mysql.connector
+import sqlite3
+
+conn = sqlite3.connect('chinook.db')
+cur = conn.cursor()
+
+music_shop_db = mysql.connector.connect(
+    host = "localhost",
+    user = "admin",
+    password = "password",
+    database = "music_shop"
+)
+
+cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+chinook_tables = cur.fetchall()
+
+cursor = music_shop_db.cursor()
+
+print(music_shop_db._open_connection)
+cursor.execute("SELECT * FROM customers")
+print(cursor.fetchall())
+
+for table in chinook_tables:
+    ch_table = table[0]
+    if 'sqlite' not in ch_table:
+        cur.execute(f"SELECT * FROM {ch_table}")
+        ch_table_content = cur.fetchall()
+        for content in ch_table_content:
+            content_list = list(content)
+            while None in content_list:
+                i = content_list.index(None)
+                content_list[i] = 0
+            content_tuple = tuple(content_list)
+            cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+            cursor.execute(f"INSERT INTO {ch_table} VALUES {content_tuple}")
+music_shop_db.commit()
+music_shop_db.close()
+
