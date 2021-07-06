78d45d59736df45b892dd8ca9fdb7c884c50875f
Chunkie0 <davisdambis1@gmail.com>
2021-07-06 21:00:31 +0300

    faster code???

--- migrate.py
+++ migrate.py
@@ -16,10 +16,8 @@ chinook_tables = cur.fetchall()
 
 cursor = music_shop_db.cursor()
 
-print(music_shop_db._open_connection)
-cursor.execute("SELECT * FROM customers")
-print(cursor.fetchall())
 
+cursor.execute("SET FOREIGN_KEY_CHECKS=0")
 for table in chinook_tables:
     ch_table = table[0]
     if 'sqlite' not in ch_table:
@@ -31,8 +29,38 @@ for table in chinook_tables:
                 i = content_list.index(None)
                 content_list[i] = 0
             content_tuple = tuple(content_list)
-            cursor.execute("SET FOREIGN_KEY_CHECKS=0")
             cursor.execute(f"INSERT INTO {ch_table} VALUES {content_tuple}")
 music_shop_db.commit()
 music_shop_db.close()
 
+# import mysql.connector
+# import sqlite3
+
+# conn = sqlite3.connect('chinook.db')
+# cur = conn.cursor()
+
+# music_shop_db = mysql.connector.connect(
+#     host = "localhost",
+#     user = "admin",
+#     password = "password",
+#     database = "music_shop"
+# )
+
+# cursor = music_shop_db.cursor()
+
+# cur.execute("SELECT name FROM sqlite_master WHERE type='table'")
+# chinook_tables = cur.fetchall()
+
+# cursor.execute("SET FOREIGN_KEY_CHECKS=0")
+
+# for table in chinook_tables:
+#     ch_table = table[0]
+#     if 'sqlite' not in ch_table:
+#         table_content = conn.execute(f"SELECT * FROM {ch_table}")
+#         columns = tuple([i[0] for i in table_content.description])
+#         values = table_content.fetchall()
+#         insert = "".join((f"INSERT INTO {ch_table} {columns} VALUES {str(values).replace('[', '').replace(']', '').replace('None', 'Null')}").split("'", len(columns)*2))
+#         cursor.execute(insert, {None:'Null'})
+        
+# music_shop_db.commit()
+# music_shop_db.close()
